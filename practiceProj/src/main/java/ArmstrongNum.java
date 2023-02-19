9cacad22d4d922548302c3c2b2cea087658733dc
Vaisingh <Vaisingh54@gmail.com>
2023-02-19 14:24:11 +0530

    Adding Armstrong class

---
+++ practiceProj/src/main/java/ArmstrongNum.java
@@ -0,0 +1,22 @@
+public class ArmstrongNum{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int n = 407,ans=0,i,real=n;
+        
+
+        while(n!=0)
+        {
+            i = n%10;
+            ans = ans + i*i*i;
+            n = n/10;
+            
+        }
+        if(real==ans)
+        System.out.println("Is Armstrong");
+        else
+        System.out.println("Not armstrong");
+        
+    }
+}
